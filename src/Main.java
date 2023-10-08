54a605d8d10b437d69bf8ed6a794ac9287c0e826
unknown <utilisateur@BBALZACTHERA04.thera.epsi.fr>
2023-10-08 15:47:14 +0200

    commit two

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        String txt = "Reponsse chaz";
+
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+    }
+}
